715846f29bd664e8afe15080626580d28935afb6
Vlad <VladDaImpaler@gmail.com>
2020-09-11 16:27:05 -0400

    minor changes, nothing notable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
     def welcome
         @pictures = Picture.all
     end
+
+    def dashboard
+        @picture = Picture.where(user_id: current_user.id)
+    end
 end
