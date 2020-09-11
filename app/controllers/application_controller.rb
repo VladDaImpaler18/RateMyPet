2fdce1b2fe6d684910ac55233f7754eaeb9cd132
vlad <VladDaImpaler@gmail.com>
2020-09-10 21:36:39 -0400

    nav bar layout, initial post idea

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
     def welcome
-
+        @pictures = Picture.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   #devise_for :views
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'callbacks'}
 
-  #nancy did this below : These are for creating shotcut routes. so localhost:3000/login goes to 'devise/sessions#new'. Must make sure links reflect change
-  # devise_scope :user do
-  #  get 'login', to: 'devise/sessions#new'
-  #  get 'signup', to: 'devise/registrations#new'
-  #  get 'signout', to:
-  
-  # end
+  nancy did this below : #These are for creating shotcut routes. so localhost:3000/login goes to 'devise/sessions#new'. Must make sure links reflect change
+   devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+    get 'signout', to:
+    get 'dashboard',
+   end
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] #Do i need?
   root to: 'application#welcome'
   
