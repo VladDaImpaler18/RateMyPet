64cb4e0a3d36a2810aaabacce6fa5cb59c7ef7ec
Vlad <VladDaImpaler@gmail.com>
2021-03-19 12:34:05 -0400

    minor fixes

--- db/migrate/20200914065146_drop_reply_and_comment_reply_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropReplyAndCommentReplyTables < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :replies
-    drop_table :comment_replies
-  end
-  def down
-    fail ActiveRecord::IrreversibleMigration #Since tables are no longer needed, no need to reverse (and recover data)
-  end
-end
